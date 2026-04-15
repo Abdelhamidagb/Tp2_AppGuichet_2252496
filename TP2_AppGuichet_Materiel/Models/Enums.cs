58891b08086b2016f05ae1673067f243b814737e
Abdelhamidagb <2252496@cegepmontpetit.ca>
2026-04-15 00:16:13 -0400

    creations de l'enums
    plusieur enums dans une seule liste pour facilite la lecture plutard

---
+++ TP2_AppGuichet_Materiel/Models/Enums.cs
@@ -0,0 +1,32 @@
+﻿namespace AppGuichet
+{
+    /// <summary>
+    /// Énumérations utilisées dans l'application du guichet bancaire
+    /// </summary>
+    public enum SorteTransactions
+    {
+        Dépôt,
+        Retrait
+    }
+
+    public enum Roles
+    {
+        Administrateur,
+        Client 
+    }
+
+    public enum SorteComptes
+    {
+        Aucun ,
+        Épargne,
+        Chèque,
+        Intérêt 
+    }
+
+    public enum FiltreOperation
+    {
+        Toutes ,
+        Dépôt ,
+        Retrait
+    }
+}
